49347521c07e703e87d060ee47258621759a9112
James Kim <james.kim7@yahoo.com>
2017-02-02 00:29:20 -0500

    reducer calculator hw finished

--- 18.2-react-review-thurs/hello-react/src/reducers/calculator.js
+++ 18.2-react-review-thurs/hello-react/src/reducers/calculator.js
@@ -4,20 +4,15 @@ let createStore = require('redux').createStore
 
 let add = "ADD";
 let subtract = "SUBTRACT";
-let divide = "DIVIDE";
 let multiply = "MULTIPLY";
+let divide = "DIVIDE";
 let clear = "CLEAR"
 
-let inputOne = 500;
-let inputTwo = 100;
-
-
 function calculator(state, action) {
-	let newState = Object.assign({}, state || {});
+	const currentState = state || 0;
+	let newState = currentState;
 	
-	let valOne = action.valOne;
-	let valTwo = action.valTwo;
-	let value = action.value;
+	let number = action.number
 
 	// if (action.type === add) {
 	// 	newState = valOne + valTwo;
@@ -33,19 +28,19 @@ function calculator(state, action) {
 
 	switch (action.type) {
 	case add:
-		newState = { value: valOne + valTwo };
+		newState += number;
 		break;
 	case subtract:
-		newState = { value: valOne - valTwo };
+		newState -= number;
 		break;
 	case divide:
-		newState = { value: valOne / valTwo };
+		newState /= number;
 		break;
 	case multiply:
-		newState = { value: valOne * valTwo };
+		newState *= number;
 		break;
 	case clear:
-		newState = { value: 0 }
+		newState = 0;
 	default:
 		return newState;
 	}
@@ -61,52 +56,16 @@ store.subscribe(() => {
 })
 
 
-const inputA = {
-	type: add,
-	value: 0,
-	valOne: inputOne,
-	valTwo: inputTwo
-}
-
-const inputB = {
-	type: subtract,
-	value: 0,
-	valOne: inputOne,
-	valTwo: inputTwo
-}
-
-const inputC = {
-	type: divide,
-	value: 0,
-	valOne: inputOne,
-	valTwo: inputTwo
-}
-
-const inputD = {
-	type: multiply,
-	value: 0,
-	valOne: inputOne,
-	valTwo: inputTwo
-}
-
-const inputE = {
-	type: clear,
-	value: 0
-}
-
-
 
 console.log("dispatching action:");
-store.dispatch(inputA);
-store.dispatch(inputE);
 
-store.dispatch(inputB);
-store.dispatch(inputE);
+store.dispatch( {type: add, number: 100} );
+store.dispatch( {type: subtract, number: 50} );
+store.dispatch( {type: multiply, number: 2} );
+store.dispatch( {type: divide, number: 2} );
+store.dispatch( {type: clear} );
+store.dispatch( {type: add, number: 42} );
 
-store.dispatch(inputC);
-store.dispatch(inputE);
 
-store.dispatch(inputD);
-store.dispatch(inputE);
 
 
